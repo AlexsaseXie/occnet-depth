80d17c287ee82b3918f8956cc2f8438d160c8680
AlexsaseXie <thss15_xieyw@163.com>
2020-04-01 13:12:15 +0800

    using batch norm in feature_extractor

--- im2mesh/onet_multi_layers_predict/models/feature_extractor.py
+++ im2mesh/onet_multi_layers_predict/models/feature_extractor.py
@@ -17,7 +17,7 @@ class Resnet18_Full(nn.Module):
         use_linear (bool): whether a final linear layer should be used
     '''
 
-    def __init__(self, c_dim, normalize=True):
+    def __init__(self, c_dim, normalize=True, batch_norm=False):
         super().__init__()
         self.normalize = normalize
         self.features = resnet.resnet18(pretrained=True)
@@ -30,6 +30,13 @@ class Resnet18_Full(nn.Module):
             self.fc3 = nn.Sequential()
             self.fc2 = nn.Sequential()
             self.fc1 = nn.Sequential()
+
+        self.batch_norm = batch_norm
+        if self.batch_norm:
+            print('Using layer norm in encdoer')
+            self.f1_bn = nn.BatchNorm1d(c_dim)
+            self.f2_bn = nn.BatchNorm1d(c_dim)
+            self.f3_bn = nn.BatchNorm1d(c_dim)
 
         self.f1_fc = nn.Linear(1568,512)
         self.f2_fc = nn.Linear(1568,512)
@@ -52,6 +59,11 @@ class Resnet18_Full(nn.Module):
         f3 = self.fc3(f3)
         f2 = self.fc2(f2)
         f1 = self.fc1(f1)
+
+        if self.batch_norm:
+            f3 = self.f3_bn(f3)
+            f2 = self.f2_bn(f2)
+            f1 = self.f1_bn(f1)
         return f3, f2, f1
 
 
@@ -63,7 +75,7 @@ class Resnet18_Local(nn.Module):
         use_linear (bool): whether a final linear layer should be used
     '''
 
-    def __init__(self, c_dim, feature_map_dim=64 ,normalize=True):
+    def __init__(self, c_dim, feature_map_dim=64 ,normalize=True, batch_norm=False):
         super().__init__()
         self.normalize = normalize
         self.features = resnet.resnet18(pretrained=True)
@@ -74,6 +86,14 @@ class Resnet18_Local(nn.Module):
             self.fc3 = nn.Sequential()
 
         self.feature_map_dim = feature_map_dim
+
+        self.batch_norm = batch_norm
+        if self.batch_norm:
+            print('Using layer norm in encdoer')
+            self.f1_bn = nn.BatchNorm1d(feature_map_dim)
+            self.f2_bn = nn.BatchNorm1d(feature_map_dim)
+            self.f3_bn = nn.BatchNorm1d(c_dim)
+
         self.f2_conv = nn.Conv1d(256, self.feature_map_dim ,1)
         self.f1_conv = nn.Conv1d(128, self.feature_map_dim ,1)
 
@@ -93,17 +113,23 @@ class Resnet18_Local(nn.Module):
         f2 = F.grid_sample(f2, points_img)
         f2 = f2.squeeze(2)
         f2 = self.f2_conv(f2)
-        f2 = f2.transpose(1, 2)
-        # f2 : batch * n_pts * fmap_dim
 
         f1 = F.relu(f1)
         f1 = F.grid_sample(f1, points_img)
         f1 = f1.squeeze(2)
         f1 = self.f1_conv(f1)
-        f1 = f1.transpose(1, 2)
-        # f1 : batch * n_pts * fmap_dim
-
+        
         f3 = self.fc3(f3)
+
+        if self.batch_norm:
+            f3 = self.f3_bn(f3)
+            f2 = self.f2_bn(f2)
+            f1 = self.f1_bn(f1)
+
+        f2 = f2.transpose(1, 2)
+        f1 = f1.transpose(1, 2)
+        # f2 : batch * n_pts * fmap_dim
+        # f1 : batch * n_pts * fmap_dim
         return f3, f2, f1
 
     def encode_first_step(self, x):
@@ -122,15 +148,21 @@ class Resnet18_Local(nn.Module):
         f2 = F.grid_sample(f2, points_img)
         f2 = f2.squeeze(2)
         f2 = self.f2_conv(f2)
-        f2 = f2.transpose(1, 2)
-        # f2 : batch * n_pts * fmap_dim
 
         f1 = F.relu(f1)
         f1 = F.grid_sample(f1, points_img)
         f1 = f1.squeeze(2)
         f1 = self.f1_conv(f1)
-        f1 = f1.transpose(1, 2)
-        # f1 : batch * n_pts * fmap_dim
-
+        
         f3 = self.fc3(f3)
+
+        if self.batch_norm:
+            f3 = self.f3_bn(f3)
+            f2 = self.f2_bn(f2)
+            f1 = self.f1_bn(f1)
+
+        f2 = f2.transpose(1, 2)
+        f1 = f1.transpose(1, 2)
+        # f2 : batch * n_pts * fmap_dim
+        # f1 : batch * n_pts * fmap_dim
         return f3, f2, f1
