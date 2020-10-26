90ded1319d66f39d56a4cee175467527f81bc020
AlexsaseXie <thss15_xieyw@163.com>
2020-10-26 03:12:51 -0400

    mask flow fix

--- im2mesh/utils/mask_flow.py
+++ im2mesh/utils/mask_flow.py
@@ -7,33 +7,46 @@ def get_borders(mask):
         returns : ndarray (n * 2) [(x,y)]
     '''
     h, w = mask.shape
-    mask_left = np.logical_not(np.concatenate((np.zeros((h, 1), dtype=bool), mask[:-1, :]), axis=0))
-    mask_right = np.logical_not(np.concatenate((mask[1:, :], np.zeros((h, 1), dtype=bool)), axis=0))
-    mask_top = np.logical_not(np.concatenate((np.zeros((1, w), dtype=bool), mask[:, :-1]), axis=1))
-    mask_bottom = np.logical_not(np.concatenate((mask[:, 1:], np.zeros((1, w), dtype=bool)), axis=1))
+    mask_left = np.logical_not(np.concatenate((np.zeros((h, 1), dtype=bool), mask[:, :-1]), axis=1))
+    mask_right = np.logical_not(np.concatenate((mask[:, 1:], np.zeros((h, 1), dtype=bool)), axis=1))
+    mask_top = np.logical_not(np.concatenate((np.zeros((1, w), dtype=bool), mask[:-1, :]), axis=0))
+    mask_bottom = np.logical_not(np.concatenate((mask[1:, :], np.zeros((1, w), dtype=bool)), axis=0))
     is_border = mask & (mask_left | mask_right | mask_top | mask_bottom)
     
     return np.argwhere(is_border)
 
-def mask_flow(mask):
+def mask_flow(mask, return_borders=False):
     '''
         mask : ndarray (H * W)
         returns : ndarray (H * W) mask_flow
     '''
     h, w = mask.shape
     mask = mask.astype(bool)
-    borders = get_borders(mask).astype(np.float32)
+    borders = get_borders(mask)
+    borders_float = borders.astype(np.float32)
     # return a float 
     new_mask = mask.astype(np.float32)
 
     to_calculate_idx = np.nonzero(mask == 0)
     to_calculate_idx_float = np.transpose(to_calculate_idx).astype(np.float32)
 
-    kdtree = KDTree(borders)
+    kdtree = KDTree(borders_float)
     dist, idx = kdtree.query(to_calculate_idx_float, k=1)
 
-    new_mask[to_calculate_idx] = 1. - dist / (np.sqrt(h * w))
-    return new_mask
+    new_mask[to_calculate_idx] = 1. -  dist / (np.sqrt(h * w))
+
+    # normalize
+    background = new_mask[to_calculate_idx]
+    background_min = np.min(background)
+    background_max = np.max(background)
+    new_mask[to_calculate_idx] = (background - background_min) / (background_max - background_min)
+
+    if return_borders:
+        borders_mask = np.zeros((h,w), dtype=bool)
+        borders_mask[borders[:,0], borders[:,1]] = 1.
+        return new_mask, borders_mask
+    else:
+        return new_mask
 
     
     
--- scripts/render_img_views/3D-R2N2/save_mask_flow.py
+++ scripts/render_img_views/3D-R2N2/save_mask_flow.py
@@ -1,9 +1,12 @@
-from ../../../im2mesh.utils.mask_flow import mask_flow
+#from ../../../im2mesh.utils.mask_flow import mask_flow
 import numpy as np
 from rendering_config import *
 
 import os
 import sys
+sys.path.append('../../../')
+from im2mesh.utils.mask_flow import mask_flow
+
 import time
 from PIL import Image
 import argparse
@@ -43,8 +46,8 @@ def main(args):
             
             mask_flow_array = mask_flow(depth_mask_array)
             
-            mask_flow_img = Image.fromarray(mask_flow_img, 'F')
-            mask_flow_img.save(os.path.join(rendering_curr_model_save_mask_flow_root, '%.2d_mask_flow.png' % view_id))
+            mask_flow_img = Image.fromarray((mask_flow_array * 255.).astype(np.uint8), 'L')
+            mask_flow_img.save(os.path.join(rendering_curr_model_save_mask_flow_root, '%.2d_mask_flow.png' % view_id))     
 
         end_time = time.time()
         print('transfer model in', end_time - start_time, ' secs')
@@ -59,7 +62,7 @@ def main_single(args):
         os.mkdir(rendering_curr_model_save_mask_flow_root)
 
     if os.path.exists(os.path.join(rendering_curr_model_save_mask_flow_root, '%.2d_mask_flow.png' % (N_VIEWS - 1))):
-        continue
+        return
     
     for view_id in range(N_VIEWS):
         mask_path = os.path.join(rendering_curr_model_save_mask_root, '%.2d_mask.png' % view_id)
@@ -69,15 +72,14 @@ def main_single(args):
         
         mask_flow_array = mask_flow(depth_mask_array)
         
-        mask_flow_img = Image.fromarray(mask_flow_img, 'F')
+        mask_flow_img = Image.fromarray((mask_flow_array * 255.).astype(np.uint8), 'L')
         mask_flow_img.save(os.path.join(rendering_curr_model_save_mask_flow_root, '%.2d_mask_flow.png' % view_id))
-        
 
 
 def test():
     n_view = 0
-    model_class = ['04090263']
-    model_id = ['4a32519f44dc84aabafe26e2eb69ebf4']
+    model_class = ['02691156']
+    model_id = ['10155655850468db78d106ce0a280f87']
     for i, curr_model_id in enumerate(model_id):
         mask_root = os.path.join(DIR_RENDERING_PATH, model_class[i], curr_model_id, 'rendering_mask')
         mask_path = os.path.join(mask_root, '%.2d_mask.png' % n_view)
@@ -85,6 +87,9 @@ def test():
         depth_mask = Image.open(mask_path).convert('1')
 
         # mkdirs
+        if not os.path.exists(TEST_RENDERING_PATH):
+            os.mkdir(TEST_RENDERING_PATH)        
+
         if not os.path.exists(os.path.join(TEST_RENDERING_PATH, model_class[i])):
             os.mkdir(os.path.join(TEST_RENDERING_PATH, model_class[i]))
 
@@ -94,15 +99,25 @@ def test():
         if not os.path.exists(os.path.join(TEST_RENDERING_PATH, model_class[i], curr_model_id, 'rendering_mask')):
             os.mkdir(os.path.join(TEST_RENDERING_PATH, model_class[i], curr_model_id, 'rendering_mask'))
 
-        os.mkdir(os.path.join(TEST_RENDERING_PATH, model_class[i], curr_model_id, 'rendering_mask_flow'))
+        if not os.path.exists(os.path.join(TEST_RENDERING_PATH, model_class[i], curr_model_id, 'rendering_mask_flow')):
+            os.mkdir(os.path.join(TEST_RENDERING_PATH, model_class[i], curr_model_id, 'rendering_mask_flow'))
 
         # save input
         depth_mask.save(os.path.join(TEST_RENDERING_PATH, model_class[i], curr_model_id, 'rendering_mask', '%.2d_mask.png' % n_view))
         depth_mask_array = np.array(depth_mask)
         
-        mask_flow_array = mask_flow(depth_mask_array)
-        
-        mask_flow_img = Image.fromarray(mask_flow_img, 'F')
+        print('depth_mask_array:',depth_mask_array.shape)
+
+        mask_flow_array, borders_array = mask_flow(depth_mask_array, True)
+        print(borders_array)
+             
+        borders_mask = Image.fromarray(borders_array.astype(np.uint8))
+        borders_mask = borders_mask.point(lambda i: i == 1, '1')
+        borders_mask.save(os.path.join(TEST_RENDERING_PATH, model_class[i], curr_model_id, 'rendering_mask_flow', '%.2d_mask.png' % n_view))
+
+        mask_flow_img = Image.fromarray(mask_flow_array, 'F')
+        mask_flow_img.save(os.path.join(TEST_RENDERING_PATH, model_class[i], curr_model_id, 'rendering_mask_flow', '%.2d_mask_flow.tiff' % n_view))
+        mask_flow_img = Image.fromarray((mask_flow_array * 255.).astype(np.uint8), 'L')
         mask_flow_img.save(os.path.join(TEST_RENDERING_PATH, model_class[i], curr_model_id, 'rendering_mask_flow', '%.2d_mask_flow.png' % n_view))
 
 if __name__ == "__main__":
