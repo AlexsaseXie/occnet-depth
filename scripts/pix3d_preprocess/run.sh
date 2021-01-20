a67d7d7a0778ee9d5d872e9f4f6a01ab268e1eb6
AlexsaseXie <thss15_xieyw@163.com>
2021-01-20 03:36:42 -0500

    fix

--- scripts/pix3d_preprocess/run.sh
+++ scripts/pix3d_preprocess/run.sh
@@ -1,7 +1,7 @@
-#mkdir -p /home2/xieyunwei/occupancy_networks/data/pix3d
+mkdir -p /home2/xieyunwei/occupancy_networks/data/pix3d
 
-#echo 'Sampling'
-#python 0_sample.py --pix3d_root /home2/xieyunwei/pix3d/ --sampled_list_root /home2/xieyunwei/occupancy_networks/data/pix3d/sampled_list/
+echo 'Sampling'
+python 0_sample.py --pix3d_root /home2/xieyunwei/pix3d/ --sampled_list_root /home2/xieyunwei/occupancy_networks/data/pix3d/sampled_list/
 
 echo 'Generating'
 python 1_generate_test_imgs.py --pix3d_root /home2/xieyunwei/pix3d/ --sampled_list_root /home2/xieyunwei/occupancy_networks/data/pix3d/sampled_list/ --output_list_root /home2/xieyunwei/occupancy_networks/data/pix3d/generation_sampled_list/ --generate_root /home2/xieyunwei/occupancy_networks/data/pix3d/generation/ --fit_focal_length --intermediate_output --test_num -1
